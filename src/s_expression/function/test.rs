b89c895ce265e38680d41d8cf7132befb24ca77e
Sean DiGirolamo <s.digirolamo218@gmail.com>
2022-07-20 21:14:49 -0400

    Added even more functions to macro and function

--- src/s_expression/function/test.rs
+++ src/s_expression/function/test.rs
@@ -49,3 +49,48 @@ pub fn args_is_none_when_passed_sxref_with_nil_args() {
 
     assert_eq!(SXRef::number(2), actual.definition())
 }
+
+#[test]
+pub fn from_string_works() {
+    let subject = "(lambda (x) (cons x '(x ())))";
+
+    let actual = Function::try_from(subject).unwrap();
+
+    assert_eq!(&vec!["x".to_owned()], actual.args());
+
+    assert_eq!(
+        SXRef::from(vec![
+            SXRef::symbol("cons".into()),
+            SXRef::symbol("x".into()),
+            SXRef::from(vec![
+                SXRef::symbol("quote".into()),
+                SXRef::from(vec![
+                    SXRef::symbol("x".into()),
+                    SXRef::nil(),
+                ]),
+            ]),
+        ]),
+        actual.definition()
+    )
+}
+
+#[test]
+pub fn str_that_are_missing_args_work() {
+    let subject = "(lambda)";
+
+    let actual = Function::try_from(subject).unwrap();
+    let empty: Vec<String> = Vec::new();
+
+    assert_eq!(&empty, actual.args());
+
+    assert_eq!(SXRef::nil(), actual.definition())
+}
+
+
+#[test]
+#[should_panic]
+pub fn bad_lisp_fails_to_become_function() {
+    let subject = "(lambda";
+
+    Function::try_from(subject).unwrap();
+}
