3d58456db9459651c905756a24e2f0596c807235
DiGirolamo,Sean <sean_digirolamo@vanguard.com>
2022-07-21 17:13:22 +0000

    Added tests for car to McCarthyScope::test. Some are failing! Make them pass

--- src/evaluate/environment/mccarthy_scope/test.rs
+++ src/evaluate/environment/mccarthy_scope/test.rs
@@ -170,3 +170,135 @@ pub fn cons_creates_new_cons_cell_when_second_arg_is_an_atom() {
 
     assert_eq!(expected, actual)
 }
+
+#[test]
+pub fn car_returns_first_item_in_list_of_2() {
+    let mut env = Env::new();
+    env.push(McCarthyScope::new());
+
+    let subject = SXRef::from(vec![
+        SXRef::symbol("car".into()),
+        SXRef::quote(
+            SXRef::from(vec![
+                SXRef::number(1),
+                SXRef::number(2),
+            ]),
+        ),
+    ]);
+
+    let expected =  SXRef::number(1);
+
+    let actual = eval(subject, &mut env);
+
+    assert_eq!(expected, actual)
+}
+
+#[test]
+pub fn car_returns_first_item_in_list_of_1() {
+    let mut env = Env::new();
+    env.push(McCarthyScope::new());
+
+    let subject = SXRef::from(vec![
+        SXRef::symbol("car".into()),
+        SXRef::quote(
+            SXRef::from(vec![
+                SXRef::number(1),
+            ]),
+        ),
+    ]);
+
+    let expected =  SXRef::number(1);
+
+    let actual = eval(subject, &mut env);
+
+    assert_eq!(expected, actual)
+}
+
+#[test]
+pub fn car_returns_nil_from_nil() {
+    let mut env = Env::new();
+    env.push(McCarthyScope::new());
+
+    let subject = SXRef::from(vec![
+        SXRef::symbol("car".into()),
+        SXRef::nil(),
+    ]);
+
+    let expected = SXRef::nil();
+
+    let actual = eval(subject, &mut env);
+
+    assert_eq!(expected, actual)
+}
+
+#[test]
+pub fn car_returns_first_item_in_list_starting_with_list() {
+    let mut env = Env::new();
+    env.push(McCarthyScope::new());
+
+    let subject = SXRef::from(vec![
+        SXRef::symbol("car".into()),
+        SXRef::quote(
+            SXRef::from(vec![
+                SXRef::from(vec![
+                    SXRef::number(1),
+                    SXRef::number(2),
+                ]),
+                SXRef::number(3),
+            ]),
+        ),
+    ]);
+
+    let expected = SXRef::from(vec![
+        SXRef::number(1),
+        SXRef::number(2),
+    ]);
+
+    let actual = eval(subject, &mut env);
+
+    assert_eq!(expected, actual)
+}
+
+#[test]
+pub fn car_returns_nil_from_list_starting_with_nil() {
+    let mut env = Env::new();
+    env.push(McCarthyScope::new());
+
+    let subject = SXRef::from(vec![
+        SXRef::symbol("car".into()),
+        SXRef::quote(
+            SXRef::from(vec![
+                SXRef::number(1),
+                SXRef::number(2),
+            ]),
+        ),
+    ]);
+
+    let expected =  SXRef::number(1);
+
+    let actual = eval(subject, &mut env);
+
+    assert_eq!(true, false)
+}
+
+#[test]
+pub fn car_returns_nil_from_atom() {
+    let mut env = Env::new();
+    env.push(McCarthyScope::new());
+
+    let subject = SXRef::from(vec![
+        SXRef::symbol("car".into()),
+        SXRef::quote(
+            SXRef::from(vec![
+                SXRef::number(1),
+                SXRef::number(2),
+            ]),
+        ),
+    ]);
+
+    let expected =  SXRef::number(1);
+
+    let actual = eval(subject, &mut env);
+
+    assert_eq!(true, false)
+}
